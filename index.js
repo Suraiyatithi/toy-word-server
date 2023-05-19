c04cbe222f9cfd02b04dab4acf18d4f898b23619
unknown <you@example.com>
2023-05-19 03:31:59 -0500

    post method worked

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 const toysCollection=client.db('toyWorld').collection('toys')
+const myCollection=client.db('toyWorld').collection('mytoys')
 app.get('/toys',async(req,res)=>{
     const cursor=toysCollection.find();
     const result=await cursor.toArray()
@@ -41,7 +42,17 @@ app.get('/toys/:id',async(req,res)=>{
   const result=await toysCollection.findOne(query);
   res.send(result)
 })
-
+app.get('/mytoys',async(req,res)=>{
+  const cursur=myCollection.find();
+  const result=await cursur.toArray();
+  res.send(result);
+})
+app.post('/mytoys',async(req,res)=>{
+  const adding=req.body;
+  console.log(adding);
+  const result=await myCollection.insertOne(adding);
+  res.send(result);
+})
 
 
 
